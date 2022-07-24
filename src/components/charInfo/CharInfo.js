5da22015ecdd4b5307e62b4e7b404e6b3532acd1
Sergey <sergeyxyzz@yandex.ru>
2022-07-24 20:26:27 +0300

    add propTypes libary

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react/cjs/react.production.min';
+import PropTypes from 'prop-types'
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton'
@@ -108,6 +109,10 @@ const View = ({char}) => {
                     }
                 </ul>
             </>)
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number // charId обязательно должен быть числом, проверка на тип
 }
 
 export default CharInfo;
